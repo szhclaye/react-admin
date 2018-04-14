e14aceaea9c89d55b5b2f3c520834b8e3633c8fd
szhclaye <328044021@qq.com>
2018-04-14 16:35:24 +0800

    sider

--- src/constants/menus.js
+++ src/constants/menus.js
@@ -1,28 +1,24 @@
+/**
+ *  Sider menus信息
+ */
 export const menus = [
-    { key: '/app/dashboard/index', title: '首页', icon: 'mobile', },
+    { key: '/app/dashboard/index', title: '首页', icon: 'home', },
     {
         key: '/app/manager', title: '资产管理', icon: 'scan',
         sub: [
             { key: '/app/manager/spins', title: '加载中', icon: '', },
-            { key: '/app/manager/modals', title: '对话框', icon: '', },
-            { key: '/app/manager/notifications', title: '通知提醒框', icon: '', },
-            { key: '/app/manager/tabs', title: '标签页', icon: '', },
-            { key: '/app/manager/banners', title: '轮播图', icon: '', },
-            { key: '/app/manager/wysiwyg', title: '富文本', icon: '', },
-            { key: '/app/manager/drags', title: '拖拽', icon: '', },
-            { key: '/app/manager/gallery', title: '画廊', icon: '', },
-            { key: '/app/manager/map', title: '地图'},
+            { key: '/app/manager/modals', title: '对话框', icon: '', },     
         ],
     },
     {
-        key: '/app/animation', title: '系统管理', icon: 'rocket',
+        key: '/app/animation', title: '系统管理', icon: 'setting',
         sub: [
             { key: '/app/animation/basicAnimations', title: '用户管理', icon: '', },
             { key: '/app/animation/exampleAnimations', title: '资产分类', icon: '', },
         ],
     },
     {
-        key: '/app/table', title: '分析报表', icon: 'copy',
+        key: '/app/table', title: '分析报表', icon: 'bar-chart',
         sub: [
             { key: '/app/table/basicTable', title: '基础表格', icon: '', },
             { key: '/app/table/advancedTable', title: '高级表格', icon: '', },
@@ -30,7 +26,7 @@ export const menus = [
         ],
     },
     {
-        key: '/app/auth', title: '用户信息', icon: 'safety',
+        key: '/app/auth', title: '用户信息', icon: 'team',
         sub: [
             { key: '/app/auth/basic', title: '基础演示', icon: '', },
             { key: '/app/auth/routerEnter', title: '路由拦截', icon: '', },
@@ -41,13 +37,6 @@ export const menus = [
         sub: [
             { key: '/app/chart/echarts', title: 'echarts', icon: '', },
             { key: '/app/chart/recharts', title: 'recharts', icon: '', },
-        ],
-    },
-    {
-        key: '/sub4', title: '页面', icon: 'switcher',
-        sub: [
-            { key: '/login', title: '登录', icon: '', },
-            { key: '/404', title: '404', icon: '', },
         ],
     },
     
--- src/routes/index.js
+++ src/routes/index.js
@@ -10,28 +10,19 @@ import AdvancedTable from '../components/tables/AdvancedTables';
 import AsynchronousTable from '../components/tables/AsynchronousTable';
 import Echarts from '../components/charts/Echarts';
 import Recharts from '../components/charts/Recharts';
-import Icons from '../components/manager/Icons';
 import Spins from '../components/manager/Spins';
 import Modals from '../components/manager/Modals';
-import Notifications from '../components/manager/Notifications';
+
 import Tabs from '../components/manager/Tabs';
-import Banners from '../components/manager/banners';
+
 import Dashboard from '../components/dashboard/Dashboard';
-import Gallery from '../components/manager/Gallery';
 import BasicAnimations from '../components/animation/BasicAnimations';
 import ExampleAnimations from '../components/animation/ExampleAnimations';
 import AuthBasic from '../components/auth/Basic';
 import RouterEnter from '../components/auth/RouterEnter';
-import Wysiwyg from 'bundle-loader?lazy!../components/manager/Wysiwyg';  // 按需加载富文本配置
-import Bundle from '../components/widget/Bundle';
 import Cssmodule from '../components/cssmodule';
-import Mapmanager from '../components/manager/map';
 
-const WysiwygBundle = (props) => (
-    <Bundle load={Wysiwyg}>
-        {(Component) => <Component {...props} />}
-    </Bundle>
-);
+
 
 export default class CRouter extends Component {
     reqmanagerreAuth = (permission, component) => {
@@ -46,21 +37,20 @@ export default class CRouter extends Component {
             <Switch>
                 <Route exact path="/app/dashboard/index" component={Dashboard} />
                 <Route exact path="/app/form/basicForm" component={BasicForm} />
+                
                 <Route exact path="/app/table/basicTable" component={BasicTable} />
                 <Route exact path="/app/table/advancedTable" component={AdvancedTable} />
                 <Route exact path="/app/table/asynchronousTable" component={AsynchronousTable} />
+                
                 <Route exact path="/app/chart/echarts" component={Echarts} />
                 <Route exact path="/app/chart/recharts" component={Recharts} />
-                <Route exact path="/app/manager/icons" component={Icons} />
+            
+
                 <Route exact path="/app/manager/spins" component={Spins} />
                 <Route exact path="/app/manager/modals" component={Modals} />
-                <Route exact path="/app/manager/notifications" component={Notifications} />
                 <Route exact path="/app/manager/tabs" component={Tabs} />
-                <Route exact path="/app/manager/banners" component={Banners} />
-                <Route exact path="/app/manager/wysiwyg" component={WysiwygBundle} />
-                <Route exact path="/app/manager/gallery" component={Gallery} />
-                <Route exact path="/app/manager/map" component={Mapmanager} />
-
+                
+                
                 <Route exact path="/app/animation/basicAnimations" component={BasicAnimations} />
                 <Route exact path="/app/animation/exampleAnimations" component={ExampleAnimations} />
 
